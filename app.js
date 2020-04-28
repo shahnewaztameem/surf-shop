6a45508ca0cef96322863a68252ed778dd851ebd
shahnewaz tameem <shahnewaztamim@gmail.com>
2020-04-28 17:29:24 +0600

    ✔️ add body-parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -16,6 +17,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
