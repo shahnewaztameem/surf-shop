54e5c97410f908f5dde4cee623faf85766034140
shahnewaz tameem <shahnewaztamim@gmail.com>
2020-04-30 20:33:37 +0600

    Add post and review model

--- models/review.js
+++ models/review.js
@@ -1,5 +1,13 @@
-/*
-Review
-- body - string
--  author - object id (ref User)
-*/
+const mongoose = require('mongoose');
+const Schema  = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+    body: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
+
