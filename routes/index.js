5e5653767bae5251ebe38250cb663900106e09a2
shahnewaz tameem <shahnewaztamim@gmail.com>
2020-05-04 19:44:08 +0600

    User Auth Add

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { postRegister } = require('../controllers/index');
+const passport = require('passport');
+const {
+  postRegister
+} = require('../controllers');
+const {
+  errorHandler
+} = require('../middleware');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Surf Shop - Home' });
+  res.render('index', {
+    title: 'Surf Shop - Home'
+  });
 });
 
 /* GET /register . */
@@ -13,7 +21,7 @@ router.get('/register', (req, res, next) => {
 });
 
 /* POST /register . */
-router.post('/register', postRegister);
+router.post('/register', errorHandler(postRegister));
 
 /* GET /login . */
 router.get('/login', (req, res, next) => {
@@ -21,8 +29,17 @@ router.get('/login', (req, res, next) => {
 });
 
 /* POST /login . */
-router.post('/login', (req, res, next) => {
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+}), (req, res, next) => {
   res.send('POST /login');
+});
+
+/* GET /logout . */
+router.get('/logout', (req, res, next) => {
+  req.logOut();
+  res.redirect('/');
 });
 
 /* GET /PROFILE . */
