3b38cc777a7566c02b74cba221de158894b22f0c
shahnewaz tameem <shahnewaztamim@gmail.com>
2020-05-05 20:25:14 +0600

    Refactor code to the RESTful convenstion

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,31 +2,31 @@ const Post = require('../models/post');
 
 module.exports = {
     // Posts index
-    async getPosts(req, res, next) {
+    async postIndex(req, res, next) {
         let posts = await Post.find({});
         res.render('posts/index', { posts });
     },
 
     // Posts new
-    newPost(req, res, next) {
+    postNew(req, res, next) {
         res.render('posts/new');
     },
 
     // Posts create
-    async createPost(req, res, next) {
+    async postCreate(req, res, next) {
         // use req.body to create a new post
         let post = await Post.create(req.body);
         res.redirect(`/posts/${post.id}`);
     },
 
     // Posts show
-    async showPost(req, res, next) {
+    async postShow(req, res, next) {
         let post = await Post.findById(req.params.id);
         res.render('posts/show', { post })
     },
 
     // Posts Edit
-    async editPost(req, res, next) {
+    async postEdit(req, res, next) {
         let post = await Post.findById(req.params.id);
         res.render('posts/edit', { post });
 
