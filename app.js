aaf9c47639d1536851fb5569f3e0ae265b7ea285
shahnewaz tameem <shahnewaztamim@gmail.com>
2020-05-05 22:34:58 +0600

    Add method to get form data

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
